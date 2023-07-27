8b72d4030e0081fb67cdf0aeb5d07adba1607cc7
Keito Takahashi <keitakah@c1r18s1.42tokyo.jp>
2023-07-27 12:49:33 +0900

    edit stack

--- stack.c
+++ stack.c
@@ -6,27 +6,10 @@
 
 typedef int data_t;
 
-//data_t stack_data[STACK_SIZE];
-//int stack_num;
-
 typedef struct {
 	int num;
 	data_t data[STACK_SIZE];
 } stack_t;
-
-/*
-int push(data_t push_data)
-{
-	if (stack_num < STACK_SIZE)
-	{
-		stack_data[stack_num] = push_data;
-		stack_num++;
-		return (SUCCESS);
-	}
-	else
-		return (FAILURE);
-}
-*/
 
 int	push(stack_t *stk, data_t push_data)
 {
@@ -40,20 +23,6 @@ int	push(stack_t *stk, data_t push_data)
 		return (FAILURE);
 }
 
-/*
-int pop(data_t *pop_data)
-{
-	if (stack_num > 0)
-	{
-		stack_num--;
-		*pop_data = stack_data[stack_num];
-		return (SUCCESS);
-	}
-	else
-		return (FAILURE);
-}
-*/
-
 int	pop(stack_t *stk, data_t *pop_data)
 {
 	if (stk->num > 0)
@@ -66,17 +35,6 @@ int	pop(stack_t *stk, data_t *pop_data)
 		return (FAILURE);
 }
 
-/*
-void stackPrint()
-{
-	int i;
-
-	printf("stack: ");
-	for (i = 0;i < stack_num; i++) printf("%3d", stack_data[i]);
-	printf("\n");
-}
-*/
-
 void stackPrint(stack_t *stk)
 {
 	int	i;
@@ -85,39 +43,6 @@ void stackPrint(stack_t *stk)
 	for (i = 0;i < stk->num; i++) printf("%3d", stk->data[i]);
 	printf("\n");
 }
-
-/*
-int main()
-{
-	int i, p;
-
-	stack_num = 0;
-	for (i = 1; i <= 5; i++)
-	{
-		push(i);
-		printf("push %2d |", i);
-		stackPrint();
-	}
-	for (i = 1; i <= 3; i++)
-	{
-		pop(&p);
-		printf("pop  %2d |", p);
-		stackPrint();
-	}
-	for (i = 6;i <= 11; i++)
-	{
-		push(i);
-		printf("push %2d :", i);
-		stackPrint();
-	}
-	while (stack_num)
-	{
-		pop(&p);
-		printf("pop  %2d :", p);
-		stackPrint();
-	}
-}
-*/
 
 int	main(void)
 {
